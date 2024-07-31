f0a17cfc009fe1c64e27a6ff9fd2c29983eb23b9
Anthony Candelino <anthcandelino@gmail.com>
2024-07-30 21:22:21 -0400

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Anthony Candelino on 2024-07-30.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
